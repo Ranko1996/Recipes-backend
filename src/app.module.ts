323e9120730828f1c9a27d3430db8e883eaf9038
Ranko1996 <62714873+Ranko1996@users.noreply.github.com>
2022-05-13 16:35:35 +0200

    Added comment files

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
   imports: [
@@ -14,6 +15,6 @@ import { PrismaModule } from './prisma/prisma.module';
     AuthModule,
     UserModule, 
     PostModule, 
-    PrismaModule],
+    PrismaModule, CommentModule],
 })
 export class AppModule {}
